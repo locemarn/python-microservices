637332f73f504c1bca268fc2e48684114d4d8ad0
Marcelo <31143542+locemarn@users.noreply.github.com>
2023-10-20 01:38:04 +0000

    addindg changes

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ async def phrase(name: str):
     return {"result": result}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host="0.0.0.0")
+# if __name__ == "__main__":
+#     uvicorn.run(app, port=8080, host="0.0.0.0")
